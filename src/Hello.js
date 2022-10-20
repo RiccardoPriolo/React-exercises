c33ab79b6706a176c92aa00679b4ee4aae34c30d
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-10-20 17:32:45 +0200

    svolgimento components-07

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Message from "./Message";
 
 class Hello extends React.Component {
   render() {
     return (
       <>
         <h1>Hello, world! </h1>
-        <p>What a beautiful day!</p>
+        <p>
+          <Message />
+        </p>
       </>
     );
   }
