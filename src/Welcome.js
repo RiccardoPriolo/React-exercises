7bfcaceb157634c8ccdd799067c5ecbf3cd0648b
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-10-20 18:06:31 +0200

    svolgimento conditional-rendering-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,16 +6,17 @@ class Welcome extends React.Component {
     return (
       <>
         <p>Welcome, {this.props.name}</p>
-        {!!this.props.age && this.props.age > 18 && this.props.age < 65 && (
-          <Age age={this.props.age} />
-        )}
+        {!!this.props.age &&
+          this.props.age > 18 &&
+          this.props.age < 65 &&
+          this.props.name === "John" && <Age age={this.props.age} />}
       </>
     );
   }
 }
 
 Welcome.defaultProps = {
-  name: <strong>Alfio</strong>,
+  name: "John",
 };
 
 export default Welcome;
