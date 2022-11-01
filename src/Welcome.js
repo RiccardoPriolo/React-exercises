d696e46d0259d074714a6314ed8673a13b29f2d8
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-11-01 22:07:54 +0100

    Svolgimento styling-components-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,11 +5,13 @@ class Welcome extends React.Component {
   render() {
     return (
       <>
-        <p>Welcome, {this.props.name}</p>
-        {!!this.props.age &&
-          // this.props.age > 18 &&
-          this.props.age < 65 &&
-          this.props.name === "John" && <Age age={this.props.age} />}
+        <div className="welcome">
+          <p>Welcome, {this.props.name}</p>
+          {!!this.props.age &&
+            // this.props.age > 18 &&
+            this.props.age < 65 &&
+            this.props.name === "John" && <Age age={this.props.age} />}
+        </div>
       </>
     );
   }
