cfa33813970e527c01ee3f25afed9f5c9c5ed2a6
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-11-04 00:42:34 +0100

    Svolgimento context-02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,3 +1,20 @@
 import { LanguageContext } from "./LanguageContext";
 
-export function DisplayLanguage() {}
+const Strings = {
+  en: {
+    CHOSENLANGUAGE: "LANGUAGE: ENGLISH",
+  },
+  it: {
+    CHOSENLANGUAGE: "LINGUA: ITALIANA",
+  },
+};
+
+export function DisplayLanguage() {
+  return (
+    <LanguageContext.Consumer>
+      {(language) => {
+        return <h1>{Strings[language].CHOSENLANGUAGE}</h1>;
+      }}
+    </LanguageContext.Consumer>
+  );
+}
