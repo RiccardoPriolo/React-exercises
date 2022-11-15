c2a94c05eb6a5ec4cb25f0bd0f4e8744d5bde2b8
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-11-15 21:54:34 +0100

    Svolgimento custom-hooks-03

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,28 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export function GitHubUser({ username = 'RiccardoPriolo' }) {
-	const url = `https://api.github.com/users/${username}`;
-	const [ data, setData ] = useState([]);
+function useGitHubUser(username) {
+  const url = `https://api.github.com/users/${username}`;
+  const [data, setData] = useState([]);
 
-	const getData = async () => {
-		const response = await axios.get(url);
-		console.log(response);
-		setData(response.data);
-	};
+  const getData = async () => {
+    const response = await axios.get(url);
+    console.log(response);
+    setData(response.data);
+  };
 
-	useEffect(
-		() => {
-			getData();
-		},
-		[ username ]
-	);
+  useEffect(() => {
+    getData();
+  }, [username]);
 
-	return (
-		<div>
-			<img src={data.avatar_url} alt="user" className="avatar" />
-			<h2>{data.name}</h2>
-			<p>{data.html_url}</p>
-		</div>
-	);
+  return {
+    data: data,
+  };
+}
+
+export function GitHubUser({ username }) {
+  const { data } = useGitHubUser(username);
+
+  return (
+    <div>
+      <img src={data.avatar_url} alt="user" className="avatar" />
+      <h2>{data.name}</h2>
+      <p>{data.html_url}</p>
+    </div>
+  );
 }
