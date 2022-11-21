3da0621253f38d34b1eab62ef1096049ec7ab215
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-11-21 20:04:00 +0100

    Svolgimento swr-03

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,23 +1,29 @@
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 function useGitHubUser(username) {
-  const { data } = useSWR(
+  const { data, mutate } = useSWR(
     username ? `https://api.github.com/users/${username}` : null,
     fetcher
   );
 
+  function handleRefreshUsers() {
+    mutate();
+  }
+
   return {
     data,
+    onRefresh: handleRefreshUsers,
   };
 }
 
 export function GitHubUser({ username }) {
-  const { data } = useGitHubUser(username);
+  const { data, onRefresh } = useGitHubUser(username);
 
   return (
     <div>
+      <button onClick={onRefresh}>Refresh users</button>
       {data && (
         <div>
           <h1>{data.name}</h1>
